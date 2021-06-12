b0f5d5c0c3170ce9b99e9badbab9bc9ce91a558d
Narmalone <62171923+Narmalone@users.noreply.github.com>
2021-06-12 15:10:23 +0200

    qsd
    qsd

--- js/scenes/level1.js
+++ js/scenes/level1.js
@@ -5,7 +5,7 @@ class level_1 extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image('IM_mouse','assets/img/mouse.png');
+        this.load.image('mouse','assets/img/mouse.png');
     }
 
        
@@ -16,15 +16,21 @@ class level_1 extends Phaser.Scene {
         const platformTiles = map.addTilesetImage('proto','platform');
         var platform = map.createLayer('platformer',platformTiles,0,0);
         platform.setCollisionByExclusion(-1, true);
-        this.player= new mouse (this,50,50,'IM_mouse','Z','Q','D');
+        this.player= new mouse (this,50,50,'mouse','Z','Q','D');
         this.physics.add.collider(this.player, platform);
+
+        // CREATION DUN CONTAINER QUI VA POURSUIVRE LE JOUEUR, LA CAM//
+        this.camerasOfTheEnd = this.add.container(game.config.width/2, game.config.height/2 +100);
+        
         this.cameras.main.startFollow(this.player)
-        // this.cameras.main.setZoom(0.1)
+        this.cameras.main.setZoom(0.5)
         this.physics.world.setBounds(0, 0, 25600, 25600);
+        console.log(this.camerasOfTheEnd)
        
     }
 
     update(){
+        this.camerasOfTheEnd.x -=1
         this.player.update();
     }
 }
