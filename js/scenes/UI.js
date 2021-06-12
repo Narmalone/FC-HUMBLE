1c63ec41fe9b3aef63477ea69c0c646e74cd0b66
Mendiboure <nicolas.mendiboure@gmail.com>
2021-06-12 15:19:35 +0200

    button

--- js/scenes/UI.js
+++ js/scenes/UI.js
@@ -7,19 +7,26 @@ class UI extends Phaser.Scene {
 
         //buttons
         //red
-        this.redButton = this.add.sprite(300,550, 'redB');
+        this.redButton = this.add.sprite(game.config.width /2 ,game.config.height / 1.2, 'redB').setScale(0.5).setOrigin(0.5,0.5);
         this.redButton.setInteractive();
         this.redButton.on('pointerdown', function () {
             console.log('red')
         }, this);
         //blue
-        this.blueButton = this.add.sprite(600,550, 'blueB');
+        this.blueButton = this.add.sprite(game.config.width /2.6,game.config.height / 1.2, 'blueB').setScale(0.5).setOrigin(0.5,0.5);
         this.blueButton.setInteractive();
         this.blueButton.on('pointerdown', function () {
             console.log('blue')
         }, this);
         //green
-        this.greenButton = this.add.sprite(908,550, 'greenB');
+        this.greenButton = this.add.sprite(game.config.width / 1.625,game.config.height / 1.2, 'greenB').setScale(0.5).setOrigin(0.5,0.5);
+        this.greenButton.setInteractive();
+        this.greenButton.on('pointerdown', function () {
+            console.log('green')
+        }, this);
+
+        //emoji
+        this.greenButton = this.add.sprite(game.config.width / 1.625,game.config.height / 1.2, 'greenB').setScale(0.5).setOrigin(0.5,0.5);
         this.greenButton.setInteractive();
         this.greenButton.on('pointerdown', function () {
             console.log('green')
