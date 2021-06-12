bc9c2fb928299291d28cf6a6d11b9120d3bd6f8f
Narmalone <62171923+Narmalone@users.noreply.github.com>
2021-06-12 13:53:49 +0200

    ajout tile

--- js/scenes/level1.js
+++ js/scenes/level1.js
@@ -1,12 +1,16 @@
 class level_1 extends Phaser.Scene {
     constructor(){
         super('level_1');
+
     }
 
     create(){
 
         const map = this.make.tilemap({key:'level_1'});
-        
+        const platformTiles = map.addTilesetImage('tilemap','platform');
+        var platform = map.createLayer('platformer',platformTiles,0,0);
+        platform.setCollisionByExclusion(-1, true);
+
         console.log('dans le niveau 1')
     }
 
